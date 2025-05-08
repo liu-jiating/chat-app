1cfcad79740d9796971b5a82d0634cf9d5975239
liu-jiating <liu.jiating@smart-group.co.jp>
2025-05-08 14:04:34 +0900

    メッセージの表示

--- src/main/java/in/tech_camp/chat_app/entity/MessageEntity.java
+++ src/main/java/in/tech_camp/chat_app/entity/MessageEntity.java
@@ -3,13 +3,14 @@ package in.tech_camp.chat_app.entity;
 import java.security.Timestamp;
 
 import lombok.Data;
+import java.util.Date;
 
 @Data
 public class MessageEntity {
   private Integer id;
   private String content;
   private String image;
-  private Timestamp createdAt;
+  private Date createdAt;
   private UserEntity user;
   private RoomEntity room; 
 }
