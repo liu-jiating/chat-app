082e1590bf5d58e9599415e53168d67c4f841b9a
liu-jiating <liu.jiating@smart-group.co.jp>
2025-05-09 10:55:13 +0900

    各フォームのテスト

--- src/main/java/in/tech_camp/chat_app/form/RoomForm.java
+++ src/main/java/in/tech_camp/chat_app/form/RoomForm.java
@@ -8,7 +8,7 @@ import lombok.Data;
 
 @Data
 public class RoomForm {
-  @NotBlank(message = "Room Name cant't be blank", groups = ValidationPriority1.class)
+  @NotBlank(message = "Room Name can't be blank", groups = ValidationPriority1.class)
   private String name;
 
   private List<Integer> memberIds;
